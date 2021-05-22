2f8b14732936b0af0f162cb52c3b3364e74f30bf
vladislav-tomashov <vladislav.tomashov@gmail.com>
2021-05-22 21:38:28 +0300

    formatting

---
+++ src/app/reverse.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: "reverse",
+})
+export class ReversePipe implements PipeTransform {
+  transform(value: string): unknown {
+    return value.split("").reverse().join("");
+  }
+}
---
+++ src/app/sort.pipe.ts
@@ -0,0 +1,28 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: "sort",
+})
+export class SortPipe implements PipeTransform {
+  transform(
+    value: Record<string, unknown>[],
+    sortBy: string
+  ): Record<string, unknown>[] {
+    const result = [...value].sort((a, b) => {
+      const aEl = a[sortBy];
+      const bEl = b[sortBy];
+
+      if (aEl < bEl) {
+        return -1;
+      }
+
+      if (aEl > bEl) {
+        return 1;
+      }
+
+      return 0;
+    });
+
+    return result;
+  }
+}
